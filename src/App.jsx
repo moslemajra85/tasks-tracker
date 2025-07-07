bc58d7ca348930c4d686aa0fcd0b0f676a41ca09
moslemajra85 <144964665+moslemajra85@users.noreply.github.com>
2025-07-08 00:11:36 +0100

    feat: implement local storage for todos and update todo ID generation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TodoList from "./components/TodoList";
 
 const App = () => {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(
+    localStorage.getItem("todos")
+      ? JSON.parse(localStorage.getItem("todos"))
+      : []
+  );
   const [todoText, setTodoText] = useState("");
   const [filter, setFilter] = useState("all");
+
+  // Load Todos From Local Storage
+  useEffect(() => {
+    const savedTodos = localStorage.getItem("todos");
+
+    if (savedTodos) {
+      setTodos(JSON.parse(savedTodos));
+    }
+  }, []);
+
+  // save todos to the local storage whenever they change
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   const filtered = todos.filter((todo) => {
     if (filter === "completed") {
@@ -25,7 +43,7 @@ const App = () => {
   const handleAdd = () => {
     if (todoText) {
       const newTodos = {
-        id: todos.length + 1,
+        id: Date.now(),
         content: todoText,
         isCompleted: false,
       };
